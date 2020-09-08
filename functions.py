a05d73658aeab671e3e30e9767aa7efaa5a6429e
Gobind Puniani <gobindpuniani@gmail.com>
2020-09-08 02:03:49 -0700

    Created and finished tutorial

---
+++ functions.py
@@ -0,0 +1,32 @@
+#Q1
+def area(width, height):
+    result = width * height
+    return result
+
+#Q2
+result = area(5, 6)
+print(result)
+
+#Q3
+print(area(12, 89))
+print(area(1390, 4560))
+
+#Q4
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+#Q5
+print(subtract(34, 12))
+print(subtract(76, 90))
+
+#Q6
+def divide(num1, num2):
+    return num1 / num2
+
+#Q7
+def add(num1, num2):
+    return num1 + num2
+
+print(add(19, 63))
+print(add(48, 70))
